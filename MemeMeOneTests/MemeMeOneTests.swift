ef76df1ef5713205f9b9d0d8a0bc4a05aa5ca305
Fernanda Araujo <nanda.arf@gmail.com>
2018-09-17 17:56:00 -0300

    Adds unit tests to view controller

--- MemeMeOneTests/MemeMeOneTests.swift
+++ MemeMeOneTests/MemeMeOneTests.swift
@@ -1,5 +1,5 @@
 //
-//  MemeMeOneTests.swift
+//  ViewControler2Tests.swift
 //  MemeMeOneTests
 //
 //  Created by Fernanda Araújo on 17/09/18.
@@ -11,28 +11,113 @@ import Nimble
 
 @testable import MemeMeOne
 
-class MemeMeOneTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class MemeMeOneTests: QuickSpec {
+    override func spec() {
+        describe("init") {
+            let viewMock = ViewControllerMock()
+            
+            it("disable shared button on beginning") {
+                viewMock.viewDidLoad()
+                
+                expect(viewMock.sharedButtonEnabled).to(beFalse())
+            }
+            
+            it("disable cancel button on beginning") {
+                viewMock.viewDidLoad()
+                
+                expect(viewMock.cancelButtonClicked).to(beFalse())
+            }
+            
+            it("subscribe from keyboard notifications") {
+                viewMock.viewWillAppear(true)
+                
+                expect(viewMock.isSubscribeToKeyboardNotifications).to(beTrue())
+            }
+            
+            context("plays a song with an effect") {                
+                it("choose a picture from album") {
+                    viewMock.pickAnImageFromAlbum((Any).self)
+                    
+                    expect(viewMock.albumButtonClicked).to(beTrue())
+                }
+                
+                it("pick a picture from camera") {
+                    viewMock.pickAnImageFromCamera((Any).self)
+                    
+                    expect(viewMock.cameraButtonClicked).to(beTrue())
+                }
+                
+                it("share image created") {
+                    let barButton = UIBarButtonItem()
+                    viewMock.shareMemedImage(sender: barButton)
+                    
+                    expect(viewMock.sharedButtonClicked).to(beTrue())
+                }
+                
+                it("cancel edit meme") {
+                    let barButton = UIBarButtonItem()
+                    viewMock.cancelEdit(sender: barButton)
+                    
+                    expect(viewMock.cancelButtonClicked).to(beTrue())
+                }
+            }
+            
+            it("unsubscribe from keyboard notifications") {
+                viewMock.viewWillDisappear(true)
+                
+                expect(viewMock.isUnsubscribeFromKeyboardNotifications).to(beTrue())
+            }
         }
     }
+}
+
+
+class ViewControllerMock: ViewController {
+    var memeImageLoad = false
+    var cameraButtonClicked = false
+    var albumButtonClicked = false
+    var sharedButtonClicked = false
+    var sharedButtonEnabled = true
+    var cancelButtonClicked = true
+    var cancelButtonEnabled = false
+    var toolBarVisible = true
+    var isSubscribeToKeyboardNotifications = false
+    var isUnsubscribeFromKeyboardNotifications = false
     
+    override func viewDidLoad() {
+        sharedButtonEnabled = false
+        cancelButtonClicked = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    override func subscribeToKeyboardNotifications() {
+        isSubscribeToKeyboardNotifications = true
+    }
+    
+    override func unsubscribeFromKeyboardNotifications() {
+        isUnsubscribeFromKeyboardNotifications = true
+    }
+    
+    override func pickAnImageFromAlbum(_ sender: Any) {
+        albumButtonClicked = true
+    }
+    
+    override func pickAnImageFromCamera(_ sender: Any) {
+        cameraButtonClicked = true
+    }
+    
+    override func shareMemedImage(sender: UIBarButtonItem) {
+        sharedButtonClicked = true
+    }
+    
+    override func cancelEdit(sender: UIBarButtonItem) {
+        cancelButtonClicked = true
+    }
 }
